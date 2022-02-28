83382cd6a0c6b394d503a4dcd6990fe019a1dc03
Samuel-Azumah <72462228+Samuel-Azumah@users.noreply.github.com>
2022-02-28 16:08:47 +0000

    Implement sample test case

---
+++ cypress/integration/tests/001.spec.js
@@ -0,0 +1,34 @@
+/**
+ * Sample Test
+ */
+
+import { getTestSpecName } from 'lib/testSpecName'
+import { defaultDevices as devices } from 'cypress/integration/_helpers/default-devices'
+
+const testSpecName = getTestSpecName(__filename)
+
+devices.forEach((device) => {
+  const targetUrl = 'https://www.youtube.com/'
+  describe(`${testSpecName} @@ Shop Login - ${device.name}`, () => {
+    const [w, h] = device.viewport
+
+    before(() => {
+      cy.viewport(w, h)
+      cy.clearCookies({ domain: null })
+    })
+
+    beforeEach(() => {
+      cy.viewport(w, h)
+    })
+
+    afterEach(function () {
+      if (this.currentTest.state === 'failed') {
+        Cypress.runner.stop()
+      }
+    })
+
+    it('Should visit page', () => {
+      cy.visit(targetUrl)
+    })
+  })
+})
